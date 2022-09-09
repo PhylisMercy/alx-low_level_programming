0bf8347e3dd08ed757a290b63347eba3adff8f85
PhylisMercy <phylis.mercy42@gmail.com>
2022-09-09 10:08:20 -0700

    fix

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -6,10 +6,20 @@
  */
 int main(void)
 {
-	int a = 0;
+	unsigned char a = '0';
+	int i;
 
-	for (; a < 10 ; a++)
+	for (i = 0; i < 10; i++)
+	{
+		putchar(a);
+		a++;
+	}
+	a = '1';
+	for (i = 0; i < 6; i++)
+	{
 		putchar('0' + a);
+		a++;
+	}
 	putchar('\n');
 
 	return (0);

79bf4401692cba7be3057823466fddb156336f3a
Phylis Mercy Njoroge <110587824+PhylisMercy@users.noreply.github.com>
2022-09-14 19:36:55 +0300

    fix

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,15 +1,22 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
-/*
- *main -  Entry point
- *Return - always success
- *
+#include "main.h"
+
+/**
+ * main - print putchar
+ * Return: always 0
  */
+
 int main(void)
 {
-	printf("_putchar\n");
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 
 	return (0);
-}
 
+}

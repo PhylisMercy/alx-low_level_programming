5d9f25c9c9b592baabc33731fd511613ddf57938
PhylisMercy <phylis.mercy42@gmail.com>
2022-09-14 09:19:48 -0700

    a function that prints the alphabet, in lowercase, followed by a new line

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -4,8 +4,6 @@
  *
  *print_alphabet - function to print abc
  *
- *
- *
  * Return: 0
  *
 */
@@ -15,8 +13,7 @@ void print_alphabet(void)
 	char c;
 
 	for (c = 'a'; c<= 'z'; c++)
-	{
 		_putchar(c);
-	}
+
 	_putchar('\n');
 }

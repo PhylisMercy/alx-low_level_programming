8b555d99244a37c27c5c0564299c8b80446dd3bf
PhylisMercy <phylis.mercy42@gmail.com>
2022-09-14 07:35:54 -0700

    a program that prints _putchar, followed by a new line

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,9 +1,9 @@
 #include <unistd.h>
 #include <putchar.h>
 /*
- * main - Entry point
- *
- * Return: Always 0 (Success)
+ * main -  Check description
+ * Description: It prints the word Holberton, followed by a new line.
+ * Return: 0.
  *
  */
 int main(void)

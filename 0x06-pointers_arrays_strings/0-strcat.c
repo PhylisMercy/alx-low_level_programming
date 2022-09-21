206e9fa77ea188aaf1b3ab38c92f4eb3686363cd
PhylisMercy <phylis.mercy42@gmail.com>
2022-09-21 08:19:08 -0700

    check code

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,9 +1,9 @@
 #include "main.h"
-/**
- *_strcat - a function that concatenates two strings.
- *@dest: an input string
- *@src: an input string
- *Return: A pointer to the resulting string
+/** 
+ * _strcat - a function that concatenates two strings.
+ * @dest: an input string
+ * @src: an input string
+ * Return: A pointer to the resulting string
  */
 char *_strcat(char *dest, char *src)
 {

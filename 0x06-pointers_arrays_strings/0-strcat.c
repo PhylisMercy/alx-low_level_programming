e22325567956b80a1db3d5fad992f688b32548fc
PhylisMercy <phylis.mercy42@gmail.com>
2022-09-22 12:30:14 -0700

    a function that concatenates two strings

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /** 
  * _strcat - a function that concatenates two strings.
  * @dest: an input string
@@ -7,14 +8,14 @@
  */
 char *_strcat(char *dest, char *src)
 {
-char *temp = dest;
+	char *temp = dest;
+	
+	while (*dest)
+		dest++;
+		
+	while (*src)
+		*dest++ = *src++;
 
-while (*dest)
-dest++;
-
-while (*src)
-*dest++ = *src++;
-
-*dest = '\0';
-return (temp);
+	*dest = '\0';
+	return (temp);
 }
